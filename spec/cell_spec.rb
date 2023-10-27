5c80ee1de9d1e84523e4cc043dd9142bae2f4d0e
Nathan Lambertson <lambertones@gmail.com>
2023-10-27 07:02:28 -0500

    Add a few more tests up to .empty?

--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -8,5 +8,22 @@ RSpec.describe Cell do
     expect(cell).to be_a(Cell)
   end
 
-  
+  it "has a coordinate attribute" do
+    cell = Cell.new("B4")
+
+    expect(cell.coordinate).to eq("B4")
+  end
+
+  it "has a ship attribute, empty by default" do
+    cell = Cell.new("B4")
+
+    expect(cell.ship).to eq(nil)
+  end
+
+  it "has an empty? method that checks if cell is empty" do
+    cell = Cell.new("B4")
+
+    expect(cell.empty?).to be true
+  end
+
 end
