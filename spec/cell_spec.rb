bbc0ccedbab8ac552bf06cc5d6a42b6deef8c213
Nathan Lambertson <lambertones@gmail.com>
2023-10-27 16:49:13 -0500

    Pull from main

--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -64,16 +64,16 @@ RSpec.describe Cell do
       expect(cell_2).to be_a(Cell)
     end
 
-    it "returns cell status"
+    it "returns cell status" do
       cell_1 = Cell.new("B4")
       cell_2 = Cell.new("C3")
-      cruiser = Ship.new("Cruiser", 3
-      
-      expect(cell_1).to eq(".")
+      cruiser = Ship.new("Cruiser", 3)
+
+      expect(cell_1.render).to eq(".")
 
       cell_1.fire_upon
 
-      expect(cell_1).to eq("M")
+      expect(cell_1.render).to eq("M")
 
       cell_2.place_ship(cruiser)
 
