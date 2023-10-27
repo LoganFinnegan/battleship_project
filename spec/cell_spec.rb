a2eca9059f8c4f826b4bef8ed679da8652e1d956
Nathan Lambertson <lambertones@gmail.com>
2023-10-27 07:09:39 -0500

    add another test for to place a ship object at cell

--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -26,4 +26,13 @@ RSpec.describe Cell do
     expect(cell.empty?).to be true
   end
 
+  it "has a method to place a ship object" do
+    cell = Cell.new("B4")
+    cruiser = Ship.new("Cruiser", 3)
+
+    cell.place_ship(cruiser)
+
+    expect(cell.empty?).to be false
+  end
+
 end
