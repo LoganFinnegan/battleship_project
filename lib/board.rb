0511d6597b6cc42bdda84c867c1680b320fa32f5
Nathan Lambertson <lambertones@gmail.com>
2023-10-28 14:16:14 -0500

    Fix up cells method and add valid_coordinate method

--- lib/board.rb
+++ lib/board.rb
@@ -1,13 +1,18 @@
 class Board
+  attr_reader :cells
   def initialize
-    @c_hash = {}
+    @cells = create_cells
   end
 
-  def cells
-    @c_hash = {
-    @c_hash['A1'] => Cell.new('A1'), @c_hash['A2'] => Cell.new('A2'), @c_hash['A3'] => Cell.new('A3'), @c_hash['A4'] => Cell.new('A4'),
-    @c_hash['B1'] => Cell.new('B1'), @c_hash['B2'] => Cell.new('B2'), @c_hash['B3'] => Cell.new('B3'), @c_hash['B4'] => Cell.new('B4'),
-    @c_hash['C1'] => Cell.new('C1'), @c_hash['C2'] => Cell.new('C2'), @c_hash['C3'] => Cell.new('C3'), @c_hash['C4'] => Cell.new('C4'),
-    @c_hash['D1'] => Cell.new('D1'), @c_hash['D2'] => Cell.new('D2'), @c_hash['D3'] => Cell.new('D3'), @c_hash['D4'] => Cell.new('D4')}
+  def create_cells
+    coords = ['A1', 'A2', 'A3', 'A4', 'B1', 'B2', 'B3', 'B4',
+    'C1', 'C2', 'C3', 'C4', 'D1', 'D2', 'D3', 'D4']
+    coords.each_with_object({}) do |coord, hash|
+      hash[coord] = Cell.new(coord)
+    end
+  end
+
+  def valid_coordinate?(coordinate)
+      @cells.keys.include?(coordinate)
   end
 end
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -11,7 +11,17 @@ RSpec.describe Board do
 
   it "can create a hash of 16 cells 4x4" do
     board = Board.new
-    
+
     expect(board.cells).to be_a(Hash)
+  end
+
+  it "can tell us if a coordinate is valid or not" do
+    board = Board.new
+    board.cells
+
+    board.valid_coordinate?('A1')
+    expect(board.valid_coordinate?('A1')).to be true
+    board.valid_coordinate?('A5')
+    expect(board.valid_coordinate?('A5')).to be false
   end
 end
