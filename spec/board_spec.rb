0383be02ea98ef771d2e43aea6d4c287c0dce9d8
Nathan Lambertson <lambertones@gmail.com>
2023-10-28 10:45:26 -0500

    Add 2 tests to kickoff It2

---
+++ spec/board_spec.rb
@@ -0,0 +1,17 @@
+require './lib/ship'
+require './lib/cell'
+require './lib/board'
+
+RSpec.describe Board do
+  it "exists" do
+    board = Board.new
+
+    expect(board).to be_a(Board)
+  end
+
+  it "has a hash of 16 cells 4x4" do
+    board = Board.new
+
+    expect(board.cells).to be_a(Hash)
+  end
+end
