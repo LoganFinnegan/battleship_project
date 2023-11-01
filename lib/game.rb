88244ac28832056b868f5ba7a725297684dba6c6
Nathan Lambertson <lambertones@gmail.com>
2023-11-01 08:56:07 -0500

    Fix some bugs and add optional colors for presentation

--- lib/game.rb
+++ lib/game.rb
@@ -1,6 +1,7 @@
 require './lib/ship'
 require './lib/cell'
 require './lib/board'
+#require 'colorize' #(gem install colorize)
 
 class Game
   attr_reader :player_board,
@@ -20,26 +21,30 @@ class Game
   end
 
   def begin_game
-    puts "+++++++++++++++++++++++++++++++++++++++++"
-    puts "Welcome to BATTLESHIP, Ahoy, Commander!!!"
-    puts "°º¤ø,..,ø¤º°`°º¤ø,.,ø¤°º¤ø,..,ø¤º°`°º¤ø,."
-    puts " ><>  ><>  ><>  ><>   ><>  ><>  ><>  ><> "
+    puts "+++++++++++++++++++++++++++++++++++++++++"#.yellow
+    puts "Welcome to BATTLESHIP, Ahoy, Commander!!!"#.red
+    puts "°º¤ø,..,ø¤º°`°º¤ø,.,ø¤°º¤ø,..,ø¤º°`°º¤ø,."#.blue
+    puts " ><>  ><>  ><>  ><>   ><>  ><>  ><>  ><> "#.grey
     puts "    Enter p to play. Enter q to quit.    "
     input = gets.chomp.downcase
     if input == "p"
-      puts "Ready your forces, Commander, Gah-Yarrr!"
+      puts "Ready your forces, Commander, Gah-Yarrr!"#.red
       sleep(1)
       setup
     elsif input == "q"
-      puts "See you next time, Commander!"
+      puts "See you next time, Commander!"#.red
       exit
     else
-      puts "Input ivalid, try again."
+      puts "Input ivalid, try again."#.red
       begin_game
     end
   end
 
   def setup
+    @player_board = Board.new
+    @computer_board = Board.new
+    @player_board.create_cells
+    @computer_board.create_cells
     npc_place_ship(@npc_submarine)
     npc_place_ship(@npc_cruiser)
     prompt_player
@@ -48,30 +53,30 @@ class Game
     player_place_ship(@player_cruiser)
     print @player_board.render(true)
     until @player_cruiser.sunk? && @player_submarine.sunk? || npc_cruiser.sunk? && npc_submarine.sunk?
-    puts "Enemy fire incoming!!!"
-    puts "~~~~~~~~~~~~~~~~~~~~~~"
+    puts "Enemy fire incoming!!!"#.red
+    puts "~~~~~~~~~~~~~~~~~~~~~~"#.blue
     sleep(2)
     npc_fire
     print @player_board.render(true)
     sleep(1)
-    puts "Player turn now, awaiting coordinate!"
-    puts "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+    puts "Player turn now, awaiting coordinate!"#.red
+    puts "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"#.blue
     sleep(0.5)
     print @npc_board.render
     player_fire
     end
     if @player_cruiser.sunk? && @player_submarine.sunk?
-      puts "Game Over.. YOU LOSE!!!"
+      puts "Game Over.. YOU LOSE!!!"#.red
       print @player_board.render
-      puts "=X=X=X=X=X=X=X=X=X=X=X="
-      puts "X=X=X=X=X=X=X=X=X=X=X=X"
-      puts "=X=X=X=X=X=X=X=X=X=X=X="
+      puts "=X=X=X=X=X=X=X=X=X=X=X="#.red
+      puts "X=X=X=X=X=X=X=X=X=X=X=X"#.red
+      puts "=X=X=X=X=X=X=X=X=X=X=X="#.red
     else
-      puts "COMMANDER! YOU HAVE WON THE BATTLE!!!"
+      puts "COMMANDER! YOU HAVE WON THE BATTLE!!!"#.red
       print @npc_board.render
-      puts "====================================="
-      puts "====================================="
-      puts "====================================="
+      puts "====================================="#.green
+      puts "====================================="#.green
+      puts "====================================="#.green
     end
     begin_game
   end
@@ -81,17 +86,17 @@ class Game
     if @npc_board.cells.keys.include?(input) && @npc_board.cells[input].fired_upon? == false
       @npc_board.cells[input].fire_upon
       if @npc_board.cells[input].empty?
-        puts "\nFire again, my liege! You have missed the target."
+        puts "\nFire again, my liege! You have missed the target."#.red
       elsif @npc_board.cells[input].ship.sunk?
-        puts "\nYOU HAVE SUNK AN ENEMY SHIP!!!"
+        puts "\nYOU HAVE SUNK AN ENEMY SHIP!!!"#.red
       elsif !@npc_board.cells[input].ship.sunk? && !@npc_board.cells[input].empty?
-        puts "\nYou have struck an enemy ship!"
+        puts "\nYou have struck an enemy ship!"#.red
       end
     elsif @npc_board.cells.keys.include?(input) && @npc_board.cells[input].fired_upon? == true
-      puts "\nYou have already fired upon that coordinate, my liege! Try again."
+      puts "\nYou have already fired upon that coordinate, my liege! Try again."#.red
       player_fire
     else
-      puts "Invalid coordinate, Sir."
+      puts "Invalid coordinate, Sir."#.red
       player_fire
     end
   end
@@ -103,11 +108,11 @@ class Game
     end
       @player_board.cells[random_cell].fire_upon
     if @player_board.cells[random_cell].empty?
-      puts "\nThe enemy missed!"
+      puts "\nThe enemy missed!"#.red
     elsif @player_board.cells[random_cell].ship.sunk?
-      puts "\nThe enemy sunk your ship!"
+      puts "\nThe enemy sunk your ship!"#.red
     else
-      puts "\nThe enemy struck your ship!"
+      puts "\nThe enemy struck your ship!"#.red
     end
   end
 
@@ -120,14 +125,14 @@ class Game
   end
 
   def prompt_player
-    puts "Enemy ships be lurking abouts.\nPick your two finest ships from the fleet.\nThe Cruiser is three units long and the Submarine is two units long.\n"
+    puts "Enemy ships be lurking abouts.\nPick your two finest ships from the fleet.\nThe Cruiser is three units long and the Submarine is two units long.\n"#.red
   end
 
   def player_place_ship(ship)
-    puts "Enter the squares for the #{ship.name} (#{ship.length} spaces):"
+    puts "Enter the squares for the #{ship.name} (#{ship.length} spaces):"#.red
     input = gets.chomp.upcase.split(' ')
     until @player_board.valid_placement?(ship, input) #&& @npc_board.cells.keys.include?(input)
-      puts "Invalid entry. Please try again."
+      puts "Invalid entry. Please try again."#.red
       input = gets.chomp.upcase.split(' ')
     end
     @player_board.place(ship, input)
